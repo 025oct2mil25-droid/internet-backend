2d849b87e6504b79a18a15a4036ca3c37cdf6a64
025oct2mil25-droid <025oct2mil25@gmail.com>
2025-10-26 02:31:23 -0500

    Create models.py

---
+++ app/app/app/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func, Text
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    items = relationship("Item", back_populates="owner")
+
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(Text, nullable=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    owner = relationship("User", back_populates="items")
